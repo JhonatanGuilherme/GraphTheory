d586f51cde7442b51ebd829af93a7d179eca2442
Jhonatan Oliveira <oliveirajhon.g@gmail.com>
2021-02-15 12:05:55 -0300

    Some corrections;

--- Roteiro 1/meu_grafo.py
+++ Roteiro 1/meu_grafo.py
@@ -1,6 +1,7 @@
 # Tratamento de erro para importações
 try:
   from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+  from bibgrafo.grafo_exceptions import *
   import itertools
   import operator
 except Exception as e:
@@ -15,24 +16,35 @@ class MeuGrafo(GrafoListaAdjacencia):
     return listaArestas
 
   def vertices_nao_adjacentes(self): # Imprimir vértices não adjacentes
+    '''
+      Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
+      Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+      :return: Uma lista com os pares de vértices não adjacentes
+    '''
     listaArestas = self.criar_lista_arestas()
     listaVertices = self.N
-    haAdjacente = True
-    for subset in itertools.combinations(listaVertices, 2):
-      if (list(subset) in listaArestas) == False:
+    listaVerticesNaoAdjacentes = []
+    for subset in itertools.permutations(listaVertices, 2):
+      if (list(subset) in listaArestas) == False and (list(subset)[::-1] in listaArestas) == False:
         a = list(subset)
-        print(a[0], "->", a[1])
-        haAdjacente = False
-    if haAdjacente:
-      print("Não há vértices adjacentes.")
+        listaVerticesNaoAdjacentes.append(a[0] + '-' + a[1])
+    return listaVerticesNaoAdjacentes
 
   def ha_laco(self): # Retornar booleano caso haja um laço
+    '''
+      Verifica se existe algum laço no grafo.
+      :return: Um valor booleano que indica se existe algum laço.
+    '''
     for chave in self.A:
       if self.A[chave].getV1() == self.A[chave].getV2():
         return True
     return False
 
   def ha_paralelas(self): # Retornar booleano caso haja uma paralela
+    '''
+      Verifica se há arestas paralelas no grafo
+      :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+    '''
     listaArestas = self.criar_lista_arestas()
     for i in range(len(listaArestas)):
       if listaArestas[i] in listaArestas[i + 1::]:
@@ -40,28 +52,44 @@ class MeuGrafo(GrafoListaAdjacencia):
     return False
 
   def grau(self, V=''): # Retorna o grau do vértice V
-    if self.verticeValido(V):  
+    '''
+      Provê o grau do vértice passado como parâmetro
+      :param V: O rótulo do vértice a ser analisado
+      :return: Um valor inteiro que indica o grau do vértice
+      :raises: VerticeInvalidoException se o vértice não existe no grafo
+    '''
+    if V in self.N:
       cont = 0
       listaArestas = self.criar_lista_arestas()
       for i in listaArestas:
-        if i[0] == V:
-          cont += 1
-        if i[1] == V:
+        if i[0] == V or i[1] == V:
           cont += 1
       return cont
-    return 0
+    raise VerticeInvalidoException
 
   def arestas_sobre_vertice(self, V): # Imprimir as arestas conectadas ao vértice V
-    if self.verticeValido(V):
+    '''
+      Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+      :param V: O vértice a ser analisado
+      :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+      :raises: VerticeInvalidoException se o vértice não existe no grafo
+    '''
+    if V in self.N:
       listaArestas = []
+      listaArestasSobreVertice = []
       for chave in self.A:
         listaArestas.append([self.A[chave].getV1(), self.A[chave].getV2(), chave])
       for i in listaArestas:
         if V in i:
-          print(i[2])
-    return 0
+          listaArestasSobreVertice.append(i[2])
+      return listaArestasSobreVertice
+    raise VerticeInvalidoException
 
   def eh_completo(self): # Retorna booleano caso grafo seja completo
+    '''
+      Verifica se o grafo é completo.
+      :return: Um valor booleano que indica se o grafo é completo
+    '''
     listaArestas = self.criar_lista_arestas()
     listaContagem = [0] * len(self.N)
     listaVertices = self.N
